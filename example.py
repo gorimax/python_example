e51e7391f2ce11cb9a8bd4e9ee1d2823bf2c7537
GORI <trek6117@gmail.com>
2025-12-25 02:58:04 +0900

    first commit

---
+++ example.py
@@ -0,0 +1,24 @@
+print('hello world')
+
+def greet():
+    print('こんにちは')
+
+greet()
+
+def print_name(name):
+    print(f'私の名前は{name}です')
+
+print_name('GORI')
+
+def get_greet():
+    return 'おはようございます'
+
+print(get_greet())
+
+def add(a ,b):
+    return a + b
+
+sum = add(1, 2)
+print(sum)
+
+
